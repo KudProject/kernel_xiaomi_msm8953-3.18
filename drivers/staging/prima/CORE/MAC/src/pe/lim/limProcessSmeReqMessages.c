8b5f2ea940af8a00c1378a7a17dbd9e02ef8c88a
Albert I <krascgq@outlook.co.id>
2017-12-23 14:31:25 +0800

    kernel: Fix all CC warnings reported by Linaro Snapshot 7.2-2017.11
    * Features fixes picked from other commits (may..be, as I'm doing
      this partial-blindly):
      https://github.com/nathanchance/angler/commit/b7d64ea0d61147289e3c892e6792a779ad41c0b7,
      https://github.com/nathanchance/op5/commit/c78a7f8a0b86f47e2a4a13aa9ddbda517d78048f,
      https://github.com/techdro-id/stormguard_kernel/commit/170cbab1e7444c66b5139e0cc357e0d4ea567e95
    * Changes from previous commit:
      - Lazily fixed all prima warnings
      - Removed -Wno flags of int-in-bool-context and format-overflow
      - No more complex comparison to fix bool-compare warning
    * Warnings handled:
      - -Wbool-compare
      - -Wbool-operation
      - -Wduplicate-decl-specifier
      - -Wint-in-bool-context
      - -Wmemset-elt-size
      - -Wmisleading-indentation
      - -Wparentheses
      - -Wswitch-unreachable
    
    Change-Id: Ia7be820a622859aae3d9eb695edc5e4bba8aace7
    Co-authored-by: Nathan Chancellor <natechancellor@gmail.com>
    Co-authored-by: Adek Maulana <adekm@techdro.id>
    Signed-off-by: Albert I <krascgq@outlook.co.id>

--- drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
+++ drivers/staging/prima/CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
@@ -190,7 +190,7 @@ __limFreshScanReqd(tpAniSirGlobal pMac, tANI_U8 returnFreshResults)
    if( (validState) && (returnFreshResults & SIR_BG_SCAN_RETURN_FRESH_RESULTS))
     return TRUE;
 
-    return FALSE;
+   return FALSE;
 }
 
 
